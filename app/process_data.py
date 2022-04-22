22d68e6041ad560feb021b61e33787e639597289
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-04-21 20:10:09 -0400

    reformat code

--- app/process_data.py
+++ app/process_data.py
@@ -2,27 +2,28 @@ def get_data():
     from requests import Request, Session
     from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
     import json
-    ticker_url='https://api.nomics.com'
-    endpoint='/v1/currencies/ticker'
+    ticker_url = 'https://api.nomics.com'
+    endpoint = '/v1/currencies/ticker'
     parameters = {
-          'key':'8381f81057e8766c11cd0109bae84864',
-          'ids': 'BTC'
-            }
-    headers={
-         'Accepts': 'application/json'
-         }
+        'key': '8381f81057e8766c11cd0109bae84864',
+        'ids': 'BTC'
+    }
+    headers = {
+        'Accepts': 'application/json'
+    }
 
     session = Session()
     session.headers.update(headers)
 
     try:
-        response = session.get(ticker_url+endpoint, params=parameters)
+        response = session.get(ticker_url + endpoint, params=parameters)
         results = json.loads(response.text)
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
-          print(e)
+        print(e)
     return results
 
+
 def process():
-    data=get_data()
+    data = get_data()
     return data[0]
