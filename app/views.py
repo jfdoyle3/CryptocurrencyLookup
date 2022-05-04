7df9a6ab75141bc4d4eb364dd78a8b9213fad014
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-05-04 19:40:47 -0400

    added render_template to call web page index.html

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+# from flask import jsonify
+from flask import render_template
 
 from app import app
 from app import process_data
@@ -6,9 +7,9 @@ from app import process_data
 
 @app.route('/')
 def index():
-    return jsonify("Welcome to CryptoSite")
+    return render_template("Welcome to CryptoSite")
 
 
 @app.route('/cryptoSymbol/<cryptoSymbol>', methods=['GET'])
 def cryptoSymbol(cryptoSymbol):
-    return jsonify(process_data.process(cryptoSymbol))
+    return render_template("index.html", data=process_data.process(cryptoSymbol))
